c5ed45cb3c636dbc5c678675a72205fb9ad6327f
Adel Ghaedi <adelghaedii12@gmail.com>
2025-06-21 23:25:34 +0330

    [add] validation quantity of service selected in appointment

--- controllers/portal_controller.py
+++ controllers/portal_controller.py
@@ -3,6 +3,7 @@ import pytz
 from datetime import datetime
 from odoo import fields
 from odoo.http import Controller,route,request
+from odoo.exceptions import ValidationError
 
 
 class AppointmentPortalController(Controller):
@@ -70,8 +71,11 @@ class AppointmentPortalController(Controller):
         utc_dt = local_dt.astimezone(pytz.UTC)
         return utc_dt
     
-    # def _check_quantity_selected_service(self,):
-    #     pass
+    def _check_quantity_selected_service(self,service_id):
+        service = request.env["appointment.service"].sudo().browse(service_id)
+        if not service.exists() or service.quantity <= 0:
+            raise ValidationError("No available quantity for the selected service.")
+
 
     @route(['/my/appointments/submit'], type='http', auth='user', website=True, csrf=True)
     def portal_submit_appointment(self, **post):
@@ -82,12 +86,12 @@ class AppointmentPortalController(Controller):
             employee_id_str = post.get('employee_id')
             service_id_str = post.get('service_id')
 
+            self._check_quantity_selected_service(int(service_id_str))
+
             naive_dt = datetime.strptime(start_datetime_str, "%Y-%m-%d %H:%M")
             start_datetime_utc = self.user_tz_to_utc(request.env, naive_dt)
             start_datetime_str_utc = fields.Datetime.to_string(start_datetime_utc)
            
-            # service = request.env['appointment.service'].sudo().browse(int(service_id_str))
-
             request.env['appointment.appointment'].sudo().create({
                 'customer_id': customer.id,
                 'service_id': int(service_id_str),
