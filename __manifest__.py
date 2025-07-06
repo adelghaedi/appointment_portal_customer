a1c72bf0b699770d75e00cdfa24448294412c8d0
Adel Ghaedi <adelghaedii12@gmail.com>
2025-07-06 14:03:12 +0330

    [update] category & summary & licence & website

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,11 @@
 {
     'name': 'Appointment Portal Customer',
     'version': '17.0.1.0.0',
-    'category': 'Portal',
-    'summary': 'Customer portal',
-    'author': 'adelghaedi',
-    'website': 'https://github.com/adelghaedi',
+    'category': 'Website',
+    'summary': 'Adds a customer portal for managing appointments, services, and employee details.',
+    'author': 'Adel Ghaedi',
+    'website': 'https://github.com/adelghaedi/appointment_portal_customer/',
+    'license': 'MIT',
     'depends': [
         'base',
         'appointment_module',
@@ -19,4 +20,5 @@
     ],
     'installable': True,
     'application': False,
+    'auto_install': False,
 }
